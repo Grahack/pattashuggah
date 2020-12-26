cb0e1e309fe6ea4380acafec712671b43123da9c
Christophe Gragnic <cgragnic@netc.fr>
2020-12-26 18:57:35 +0100

    Simplify main view

--- src/cljs/pattashuggah/views.cljs
+++ src/cljs/pattashuggah/views.cljs
@@ -1,11 +1,8 @@
 (ns pattashuggah.views
   (:require
    [re-frame.core :as re-frame]
-   [pattashuggah.subs :as subs]
    ))
 
 (defn main-panel []
-  (let [name (re-frame/subscribe [::subs/name])]
-    [:div
-     [:h1 "Hello from " @name]
-     ]))
+  [:div
+    [:h1 "Pattashuggah"]])
